8048859c01be2bfa2e35d9b550950a9cc0f4216e
Reed <reed.hunsaker@gmail.com>
2024-06-12 22:35:27 -0500

    Add Bird object

--- SDLBird/src/main.cpp
+++ SDLBird/src/main.cpp
@@ -7,10 +7,17 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include "Bird.hpp"
 
 SDL_Window *window;
 SDL_Renderer *renderer;
+Bird bird = Bird();
 int gameIsRunning;
+Uint64 lastFrameTime = 0;
+
+#define FPS 60
+#define FRAME_TARGET_TIME (1000 / FPS)
 
 int createWindow(void) {
     if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
@@ -20,13 +27,14 @@ int createWindow(void) {
     
     window = SDL_CreateWindow("SDL Bird", 680, 480, 0);
     if (!window) {
-        fprintf(stderr, "Error setting up window");
+        SDL_Log("Error setting up window: %s", SDL_GetError());
         return 0;
     }
+    
     renderer = SDL_CreateRenderer(window, NULL);
     
     if (!renderer) {
-        fprintf(stderr, "Error setting up renderer");
+        SDL_Log("Error setting up renderer: %s", SDL_GetError());
         return 0;
     }
     
@@ -34,7 +42,7 @@ int createWindow(void) {
 }
 
 void setup(void) {
-    
+    bird.setup(window, renderer);
 }
 
 /// Polls for user input and fires associated events
@@ -46,18 +54,32 @@ void pollInput(void) {
         case SDL_EVENT_QUIT:
             gameIsRunning = 0;
             break;
+        case SDL_EVENT_KEY_DOWN: case SDL_EVENT_FINGER_DOWN:
+            bird.addUpwardVelocity();
+            break;
     }
 }
 
 void update(void) {
-    
+    // Delays a set amount of time before continuing to render (standardizes FPS)
+    SDL_Delay((Uint32) FRAME_TARGET_TIME);
+    lastFrameTime = SDL_GetTicks();
+    bird.update();
 }
 
 void render(void) {
     SDL_SetRenderDrawColor(renderer, 133, 43, 255, 0);
-    SDL_RenderClear(renderer);
     
-    SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer); //
+    
+    bird.render(renderer);
+    
+    SDL_RenderPresent(renderer); // Switches the back buffer with the screen to render it
+}
+
+void destroy(void) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 }
 
 int main(int argc, char *argv[]) {
@@ -70,5 +92,7 @@ int main(int argc, char *argv[]) {
         update();
         render();
     }
+    
+    destroy();
     return 0;
 }
